d72382e405737514f92b9971e7b5e37657585e14
Sinal Kumar <sinal@allsite.ai>
2022-10-06 07:36:40 +1300

    Clarify assumption on UTM zones

--- take-home-surveyor-travel/optimise_travel.py
+++ take-home-surveyor-travel/optimise_travel.py
@@ -2,6 +2,7 @@ import math
 import utm # Coordinates are converted to UTM format for analysis in metres (assume they are in same zone)
 
 # Take a list of lat/lon coords and extract out the easting and northing vectors (in m)
+# NOTE: For the purposes of this exercise we can ignore differing zone letters and numbers
 def convert_to_utm_xy(coordinates):
     return [utm.from_latlon(*c)[:2] for c in coordinates]
 
