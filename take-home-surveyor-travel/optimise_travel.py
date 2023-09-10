fa54dd59977897dfb3f2ff78be03aec4a098d877
bryantwilliam <gogobebe2@gmail.com>
2023-09-10 16:04:46 +1200

    2 passed, 3 failed. Can be improved

--- take-home-surveyor-travel/optimise_travel.py
+++ take-home-surveyor-travel/optimise_travel.py
@@ -20,30 +20,31 @@ def calc_distance(coordinate1, coordinate2):
     return math.sqrt(dx*dx + dy*dy)
 
 def sorted_indices(coordinates, starting_index):
-    def sorted_coords(coordinates, starting_index):
-        temp_coords = coordinates.copy()
-        # Sorts the list by it's distance to the provided starting index
-        temp_coords.sort(key=lambda coord : calc_distance(coordinates[starting_index], coord)) 
-        return temp_coords
+    # TODO Still not optimal
+    indices = list(range(len(coordinates)))
     sorted_indices = []
-    for coord in sorted_coords(coordinates, starting_index):
-        # TODO add each index of a duplicate (can't simply remove them because there are len() tests).
-        #      Remember for each duplicate coord, this for loop will repeat
-        sorted_indices.append(coordinates.index(coord))
+    while True:
+        indices.sort(key=lambda index : calc_distance(coordinates[starting_index], coordinates[index]))
+        sorted_indices += indices[:2]
+        indices = indices[2 - len(indices):]
+        starting_index = sorted_indices[-1]
+        if (len(sorted_indices) == len(coordinates)):
+            break
     return sorted_indices
 
 
 def optimise_travel_order(coordinates):
     coordinates_xy = convert_to_utm_xy(coordinates)
     indices = list(range(len(coordinates_xy)))
+    optimal_start_index = 0
 
     ############ DEBUG ############
-    for index in indices:
-        print("Coordinates: ", coordinates[index])
-        print("Distance to start-point (index 0): ", calc_distance(coordinates[0], coordinates[index]))
+    print("####### BEFORE #######")
+    print("Index starting at: ", optimal_start_index)
+    debug(indices, coordinates, coordinates_xy, optimal_start_index)
     ###############################
 
-    optimal_start_index = 0
+
     smallest_distance = -1
     for index in indices: # Tries every index for the best starting index to sort the list by.
         dist = calc_total_distance(coordinates_xy, sorted_indices(coordinates, index))
@@ -54,12 +55,15 @@ def optimise_travel_order(coordinates):
     optimal_order_indices = sorted_indices(coordinates, optimal_start_index)
 
     ############ DEBUG ############
-    print("-----------------")
+    print("####### AFTER #######")
     print("Optimal start index: ", optimal_start_index)
-    for index in optimal_order_indices:
-        print("Index: ", index)
-        print("Sorted coordinates: ", coordinates[index])
-        print("Distance to start-point: ", calc_distance(coordinates[optimal_start_index], coordinates[index]))
+    debug(optimal_order_indices, coordinates, coordinates_xy, optimal_start_index)    
     ###############################  
         
     return optimal_order_indices
+
+def debug(indices, coordinates, coordinates_xy, optimal_start_index):
+    for index in indices:
+        print("Index: ", index)
+        print("Coordinates: ", coordinates[index])
+    print("Total distance: ", calc_total_distance(coordinates_xy, sorted_indices(coordinates, optimal_start_index)))
